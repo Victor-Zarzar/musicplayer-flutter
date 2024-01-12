fab563ff3e89d9536c1b45c5f9c4f98d49c328ed
victorzarzar <victorzarzar58@gmail.com>
2024-01-12 07:04:06 -0300

    implement assets and create playlistprovider

---
+++ lib/models/playlist_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:musicplayer_flutter/models/song.dart';
+
+class PlaylistProvider extends ChangeNotifier {
+  final List<Song> _playlist = [
+    Song(
+        songName: "Children of The Sea",
+        artistName: "Black Sabbath",
+        albumArtImagePath: "assets/images/heavenandhell.jpg",
+        audioPath: "audio/ Children Of The Sea.mp3"),
+    Song(
+        songName: "Eagle Fly Free",
+        artistName: "Helloween",
+        albumArtImagePath: "assets/images/helloween.jpg",
+        audioPath: "audio/Eagle Fly Free.mp3"),
+    Song(
+        songName: "Fear Of The Dark",
+        artistName: "Iron Maiden",
+        albumArtImagePath: "assets/images/fearofthedark.jpg",
+        audioPath: "audio/Fear of the Dark.mp3"),
+  ];
+
+  int? _currentSongIndex;
+
+  List<Song> get playlist => _playlist;
+  int? get currentSongIndex => _currentSongIndex;
+}
