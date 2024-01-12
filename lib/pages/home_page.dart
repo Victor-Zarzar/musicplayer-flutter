e06b7616daec673574f8689245e4b143b121a4ce
victorzarzar <victorzarzar58@gmail.com>
2024-01-12 07:15:04 -0300

    corrections in pubspec, main.dart, and homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:musicplayer_flutter/components/my_drawer.dart';
+import 'package:musicplayer_flutter/models/playlist_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -10,6 +12,11 @@ class HomePage extends StatelessWidget {
       backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(title: const Text("PLAYLIST")),
       drawer: const MyDrawer(),
+      body: Consumer<PlaylistProvider>(
+        builder: (context, value, child) => ListView.builder(
+          itemBuilder: (context, index) => const ListTile(),
+        ),
+      ),
     );
   }
 }
