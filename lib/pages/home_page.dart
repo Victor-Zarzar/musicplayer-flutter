9b6f60b6499fab5e368be9dc962191c551a4b85c
victorzarzar <victorzarzar58@gmail.com>
2024-01-12 07:31:22 -0300

    corrections in home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:musicplayer_flutter/components/my_drawer.dart';
 import 'package:musicplayer_flutter/models/playlist_provider.dart';
+import 'package:musicplayer_flutter/models/song.dart';
 import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(title: const Text("PLAYLIST")),
+      appBar: AppBar(title: const Text("P L A Y L I S T")),
       drawer: const MyDrawer(),
-      body: Consumer<PlaylistProvider>(
-        builder: (context, value, child) => ListView.builder(
-          itemBuilder: (context, index) => const ListTile(),
-        ),
-      ),
+      body: Consumer<PlaylistProvider>(builder: (context, value, child) {
+        final List<Song> playlist = value.playlist;
+
+        return ListView.builder(
+          itemCount: playlist.length,
+          itemBuilder: (context, index) {
+            final Song song = playlist[index];
+
+            return ListTile(
+              title: Text(song.songName),
+              subtitle: Text(song.artistName),
+              leading: Image.asset(song.albumArtImagePath),
+            );
+          },
+        );
+      }),
     );
   }
 }
